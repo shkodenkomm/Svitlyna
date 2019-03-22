4d1dd16895b0ac487cc1a83d0845c4c1ff3a7bed
Шкоденко Михаил <shkodenko@rozetka.com.ua>
2019-03-22 10:56:47 +0200

    первый коммит-пуш; инциализация карусели в JS - callback on load k

---
+++ js/langUtils.js
@@ -0,0 +1,39 @@
+var dic ={
+            logoText:{
+                ua:"Світлина",
+                ru:"Фото",
+                en:"Svitlyna"
+            },
+            mainPage :{
+                ua:"Головна",
+                ru:"Главная",
+                en:"Main"
+            },
+            lblPortfolio:{
+                ua:"Портфоліо",
+                ru:"Портфолио",
+                en:"Portfolio"},
+            lblContacts:{
+                ua:"Контакти",
+                ru:"Контакты",
+                en:"Contacts"},
+            lblArticles:{
+                ua:"Статті",
+                ru:"Статьи",
+                en:"Articles"
+            }
+        };
+
+
+
+function trate(lang){
+
+   $("[lanId]").each(function (index, element) {
+       var id = $(element).attr('lanId');
+       console.log("lid "+id);
+       $(element).text(dic[id][lang]);
+   });
+
+}
+
+
