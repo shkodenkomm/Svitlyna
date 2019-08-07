f73cbc8d24b64d4cb1954d4f735a1414b1c90691
shkodenko <shkodenko@rozetka.com.ua>
2019-08-07 13:38:31 +0300

    первый вариант оформления контактов; ссилки на месседжеры

--- contacts.php
+++ contacts.php
@@ -167,42 +167,70 @@
         <section class="row">
             <div class="col-2 col-s-0 col-m-0"><p></p></div>
             <div class="contakts col-8 col-s-12 " >
-                <div class="lined-caption">
-                    <hr/>
-                    <div>Давайте знакомится</div>
-                    <hr/>
+                <div class="col-2"><p></p></div>
+                <div class="col-8">
+                    <div class="lined-caption">
+                        <hr/>
+                        <h2 style="white-space: nowrap;" >Давайте знакомится</h2>
+                        <hr/>
+                    </div>
+                    <article>
+                        <p class="text1">Позвольте представится - Елена Хоменко, детский и семейный фотограф. Я очень люблю свою работу, поскольку она дарит мне возможность запечатлевать красоту окружающего мира и людей, их радость и искренние положительные эмоции. Я радуюсь вместе с моими героями и чувствую важность моей работы, поскольку фото память поможет им проживать заново неповторимые моменты их жизни и будет важным наследием для их будущих поколений.</p>
+                        <p style="margin-bottom: 0">Я работаю в таких направлениях:</p>
+                        <ul class="types">
+                            <li>Семейная фотосессия и love story;</li>
+                            <li>Детская фото сессия;</li>
+                            <li>Портретная фотография;</li>
+                            <li>Фотосессия с лошадями;</li>
+                            <li>Таинство венчания и крещения;</li>
+                            <li>Фото истории;</li>
+                            <li>Фотосьемка праздников.</li>
+                        </ul>
+                        <p class="text1">Я помогу с созданием образов, выбором локации, сценарием, позированием. 
+                        Фото получаются красивые, душевные и немного сказочные. Вместе мы создадим фото историю для вас и про вас! </p>
+                    </article>               
                 </div>
-                <article>
-                    <p>Позвольте представится - Елена Хоменко, детский и семейный фотограф. Я очень люблю свою работу, поскольку она дарит мне возможность запечатлевать красоту окружающего мира и людей, их радость и искренние положительные эмоции. Я радуюсь вместе с моими героями и чувствую важность моей работы, поскольку фото память поможет им проживать заново неповторимые моменты их жизни и будет важным наследием для их будущих поколений.</p>
-                    <p>Я работаю в таких направлениях:</p>
-                    <ul>
-                        <li>Семейная фотосессия и love story;</li>
-                        <li>Детская фото сессия;</li>
-                        <li>Портретная фотография;</li>
-                        <li>Фотосессия с лошадями;</li>
-                        <li>Таинство венчания и крещения;</li>
-                        <li>Фото истории;</li>
-                        <li>Фотосьемка праздников.</li>
-                    </ul>
-                    <p>Я помогу с созданием образов, выбором локации, сценарием, позированием. 
-                    Фото получаются красивые, душевные и немного сказочные. Вместе мы создадим фото историю для вас и про вас! </p>
-                </article>
-                <div class="lined-caption">
-                    <hr/>
-                    <h2>Контакты</h2>
-                    <hr/>
-                </div>
-                <article>
-                    <p>Вы можете задать мне все интересующие вас вопросы по телефону либо в личных сообщениях!</p>
-                    <p class="phone">097 994 44 80<span class="messeger_name">(viber, telegram)</span></p>
-                    <p class="social_link facebook">
-                        <a href="https://www.facebook.com/ElenaKhomenkoPhoto">Facebook</a>
-                        <a href="https://www.instagram.com/elena_khomenko_photo">Instagram</a>
-                    </p>
-                </article>
+                <div class="col-2"><p></p></div>
             </div>
             <div class="col-2 col-s-0 col-m-0"><p></p></div>
-        </section>        
+        </section>      
+
+        <section class="row">
+            <div class="col-2 col-s-0 col-m-0"><p></p></div>
+            <div class="contakts col-8 col-s-12" >  
+                <div class="col-2"><p></p></div>
+                <div class="col-8">
+                    <div class="lined-caption">
+                        <hr/>
+                        <h2>Контакты</h2>
+                        <hr/>
+                    </div>
+                    <article>
+                        <p style="margin-top: 0">Вы можете задать мне все интересующие вас вопросы по телефону либо в личных сообщениях!</p>
+                        <p><a href="mailto:mail@svitlyna.in.ua">Написать письмо на mail@svitlyna.in.ua</a></p>
+                        
+                        <p class="phone">+38 097 99 444 80</p>
+
+                        <p class="viber_desctop">
+                            <a title="Viber" href="viber://chat?number=+380979944480">Viber</a>
+                        </p>
+                         
+                        <p class="viber_mobile">
+                            <a title="Viber" href="viber://add?number=+380979944480">Viber</a>
+                        </p>
+    
+                        <a title="Telegram" href="tg://resolve?domain=Homenko Olena">Telegram</a>
+
+                        <p class="social">
+                            <a class="facebook" href="https://www.facebook.com/ElenaKhomenkoPhoto">Facebook</a>
+                            <a class="instagram" href="https://www.instagram.com/elena_khomenko_photo">Instagram</a>
+                        </p>
+                    </article>
+                </div>
+                <div class="col-2"><p></p></div>
+            </div>
+            <div class="col-2 col-s-0 col-m-0"><p></p></div>
+        </section >
     </main>
 </body>
 </html>
