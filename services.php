cdaeab6b003a15a0a443b4a2a7b0d2a73561bed9
shkodenko <shkodenko@rozetka.com.ua>
2019-08-13 15:19:24 +0300

    контакты с фотографией

--- services.php
+++ services.php
@@ -9,7 +9,7 @@
 <html lang="en">
 <head>
     <?php  echo $config->gtm["gan_head"];?> 
-    <title>Контакты</title>
+    <title>Услуги</title>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
